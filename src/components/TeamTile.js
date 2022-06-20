e42a13077da79270909ba019bc40bdb53c914dde
czen0002 <zc114789@gmail.com>
2022-06-20 23:46:18 +1000

    [home-page][CHENG] create home page

--- src/components/TeamTile.js
+++ src/components/TeamTile.js
@@ -3,11 +3,11 @@ import { Card, Button } from 'react-bootstrap'
 
 export default function TeamTile({teamName}) {
   return (
-    <Card style={{ width: '18rem' }} className="mx-1 my-1">
+    <Card style={{ width: '16rem' }} className="mx-1 my-1">
       <Card.Img variant="top" src={`/images/${teamName}.png`} />
       <Card.Body>
-        <Card.Title>{teamName}</Card.Title>
-        <Button>Matches</Button>
+      <Card.Title>{teamName}</Card.Title>
+      <Button>Matches</Button>
       </Card.Body>
     </Card>
   )
--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,7 +1,160 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { Container, Row } from 'react-bootstrap'
+import TeamTile from '../components/TeamTile'
 
 export default function HomePage() {
+  
+  // const [teams, setTeams] = useState([]);
+
+  // useEffect(() => {
+  //   const fetchAllTeams = async () => {
+  //     try {
+  //       const response = await fetch('http://localhost:8080/v1/team');
+  //       if (response.status !== 200) {
+  //         // do nothing
+  //       } else {
+  //         const data = await response.json();
+  //         console.log(data);
+  //         setTeams(data)
+  //       }
+  //     } catch (err) {
+  //       console.log(err);
+  //     }
+  //   }
+  //   fetchAllTeams();
+  // }, [])
+
+  const teams = [
+    {
+        "teamName": "Arsenal",
+        "teamId": 1
+    },
+    {
+        "teamName": "Aston Villa",
+        "teamId": 2
+    },
+    {
+        "teamName": "Bournemouth",
+        "teamId": 3
+    },
+    {
+        "teamName": "Brentford",
+        "teamId": 4
+    },
+    {
+        "teamName": "Brighton",
+        "teamId": 5
+    },
+    {
+        "teamName": "Burnley",
+        "teamId": 6
+    },
+    {
+        "teamName": "Cardiff",
+        "teamId": 7
+    },
+    {
+        "teamName": "Chelsea",
+        "teamId": 8
+    },
+    {
+        "teamName": "Crystal Palace",
+        "teamId": 9
+    },
+    {
+        "teamName": "Everton",
+        "teamId": 10
+    },
+    {
+        "teamName": "Fulham",
+        "teamId": 11
+    },
+    {
+        "teamName": "Huddersfield",
+        "teamId": 12
+    },
+    {
+        "teamName": "Hull",
+        "teamId": 13
+    },
+    {
+        "teamName": "Leeds",
+        "teamId": 14
+    },
+    {
+        "teamName": "Leicester",
+        "teamId": 15
+    },
+    {
+        "teamName": "Liverpool",
+        "teamId": 16
+    },
+    {
+        "teamName": "Man City",
+        "teamId": 17
+    },
+    {
+        "teamName": "Man United",
+        "teamId": 18
+    },
+    {
+        "teamName": "Middlesbrough",
+        "teamId": 19
+    },
+    {
+        "teamName": "Newcastle",
+        "teamId": 20
+    },
+    {
+        "teamName": "Norwich",
+        "teamId": 21
+    },
+    {
+        "teamName": "Sheffield United",
+        "teamId": 22
+    },
+    {
+        "teamName": "Southampton",
+        "teamId": 23
+    },
+    {
+        "teamName": "Stoke",
+        "teamId": 24
+    },
+    {
+        "teamName": "Sunderland",
+        "teamId": 25
+    },
+    {
+        "teamName": "Swansea",
+        "teamId": 26
+    },
+    {
+        "teamName": "Tottenham",
+        "teamId": 27
+    },
+    {
+        "teamName": "Watford",
+        "teamId": 28
+    },
+    {
+        "teamName": "West Brom",
+        "teamId": 29
+    },
+    {
+        "teamName": "West Ham",
+        "teamId": 30
+    },
+    {
+        "teamName": "Wolves",
+        "teamId": 31
+    }
+]
   return (
-    <h2 style={{color: 'white'}}>Home</h2>
+    <Container>
+      <Row>
+        {teams.map(team => <TeamTile key={team.teamId} teamName={team.teamName} />)}
+      </Row>
+    </Container>
   )
 }
