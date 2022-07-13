7cc8caaaa5ad5330a53d14fac521ed1955f18f3f
czen0002 <zc114789@gmail.com>
2022-07-13 21:55:21 +1000

    [result-page][CHENG] Remove unused images

--- src/pages/ResultsPage.js
+++ src/pages/ResultsPage.js
@@ -1,7 +1,40 @@
 import React from 'react'
+import { Container, DropdownButton, Dropdown } from 'react-bootstrap'
 
 export default function ResultsPage() {
   return (
-    <h2 style={{color: 'white'}}>Results</h2>
+    <>
+      <Container>
+        <br></br>
+        <ul>
+          <li>
+            <div className="jumbotron jumbotron-fluid" style={{backgroundColor: 'gray'}}>
+              <div className="container">
+                <h1 className="display-4">Fluid jumbotron</h1>
+                <p className="lead">This is a modified jumbotron that occupies the entire horizontal space of its parent.</p>
+              </div>
+            </div>
+          </li>
+          <li>
+            <div className="jumbotron jumbotron-fluid" style={{backgroundColor: 'gray'}}>
+              <div className="container">
+                <h1 className="display-4">Fluid jumbotron</h1>
+                <p className="lead">This is a modified jumbotron that occupies the entire horizontal space of its parent.</p>
+              </div>
+            </div>
+          </li>
+          <li>
+            <div className="jumbotron jumbotron-fluid" style={{backgroundColor: 'gray'}}>
+              <div className="container">
+                <h1 className="display-4">Fluid jumbotron</h1>
+                <p className="lead">This is a modified jumbotron that occupies the entire horizontal space of its parent.</p>
+              </div>
+            </div>
+          </li>
+        </ul>
+      </Container> 
+    </>
+    
+
   )
 }
