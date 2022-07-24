7069150c8966ce476fd2a1b13054174d66c7ec10
czen0002 <zc114789@gmail.com>
2022-07-24 22:39:48 +1000

    [result-page][CHENG] Display match date

--- src/pages/ResultsPage.js
+++ src/pages/ResultsPage.js
@@ -1,6 +1,6 @@
-import React, { useEffect, useRef, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useLocation } from 'react-router-dom';
-import { Container, DropdownButton, Dropdown, Button } from 'react-bootstrap';
+import { Container, DropdownButton, Dropdown, Button, Collapse } from 'react-bootstrap';
 
 export default function ResultsPage() {
 
@@ -17,6 +17,29 @@ export default function ResultsPage() {
   const [teams, setTeams] = useState([]);
   const [matches, setMatches] = useState([]);
   const [flag, setFlag] = useState(true);
+
+  const createMatchDic = (matchArray) => {
+    const matchDic = {};
+    for (const match of matchArray) {
+      if (!(match['date'] in matchDic)) {
+        matchDic[match['date']] = [match];
+      } else {
+        matchDic[match['date']].push(match);
+      }
+    }
+    return matchDic;
+  }
+
+  const createArrayFromDic = (matchDic) => {
+    const array = [];
+    for (const [key, value] of Object.entries(matchDic)) {
+      array.push({
+        date: key,
+        matchArray: value
+      })
+    }
+    return array;
+  }
   
   useEffect(() => {
     const fetchMatchesBySeason = async () => {
@@ -148,20 +171,35 @@ export default function ResultsPage() {
         }}>Reset</Button>
         <br></br>
         <br></br>
-        {matches.map((match, index) => {
-          return (
-            <ul style={{listStyleType: 'none'}}>
-              <li>
-                <div  key={index} className="jumbotron jumbotron-fluid" style={{backgroundColor: 'gray'}}>
-                  <div className="container">
-                    <h1 className="display-4">Match {match['date']}</h1>
-                    <p className="lead">{match['homeTeam']} vs {match['awayTeam']}</p>
-                  </div>
-                </div>
-              </li>
-            </ul>
-          )
-        })}
+        {
+          createArrayFromDic(createMatchDic(matches)).map((matchesObj, index) => {
+            const matchesOnDate = matchesObj['matchArray'];
+            return (
+              <div key={index}>
+                <p className='display-4' style={{color: 'white'}}>{matchesObj['date']}</p>
+                {
+                  matchesOnDate.map((match, index) => {
+                    return (
+                      <ul key={index} style={{listStyleType: 'none'}}>
+                        <li key={index} >
+                          <div 
+                          className="jumbotron jumbotron-fluid" 
+                          style={{backgroundColor: 'gray'}} 
+                          >
+                            <div className="container">
+                              <h1 className="display-4">Match {match['date']}</h1>
+                              <p className="lead">{match['homeTeam']} vs {match['awayTeam']}</p>
+                            </div>
+                          </div> 
+                        </li>
+                      </ul>
+                    )
+                  })
+                }     
+              </div> 
+            )
+          })
+        }
       </Container> 
     </>
   
