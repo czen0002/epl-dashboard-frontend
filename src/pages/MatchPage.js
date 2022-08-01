3b3932b2f5fae28cb743c168251304bb74da2995
czen0002 <zc114789@gmail.com>
2022-08-01 23:35:56 +1000

    [match-page][CHENG] Update match page

--- src/pages/MatchPage.js
+++ src/pages/MatchPage.js
@@ -3,43 +3,47 @@ import { Button, Container, Image } from 'react-bootstrap';
 import { useLocation, useNavigate } from 'react-router-dom';
 import MatchBoard from '../components/MatchBoard';
 import "./MatchPage.css";
+import { stadiums } from '../constants';
 
 export default function MatchPage() {
 
   const location = useLocation();
   const match = location.state.match;
+  const team = location.state.team;
   const month = location.state.month;
   const season = location.state.season;
   const navigate = useNavigate();
   const backToResultPage = () => {
     navigate("/results" , {
       state: {
-        team: match['homeTeam'],
+        team: team,
         month: month,
         season: season
       }
     });
   };
 
+  console.log(team);
+
   return (
     <Container>
       <br></br>
       <div id='matchTitle'>
-        <div>
+        <div id='matchLogo'>
           <Image src={`/images/${match['homeTeam']}.png`} fluid />
           <p className='my-auto text-light'>{match['homeTeam']}</p>
         </div>
-        <div id='matchCenter' style={{backgroundColor: '#565656', borderRadius: '6px'}}>
-          <div id='rowOne'>{match['date']}</div>
-          <div id='rowTwo'><p>Wembley Stadium</p></div>
-          <div id='rowThree'><p>Referee</p></div>
+        <div id='matchCenter'>
+          <div id='rowOne' className="fs-5">{match['date']}</div>
+          <div id='rowTwo' className="fs-5">{stadiums[match['homeTeam']]}</div>
+          <div id='rowThree' className="fs-5">{match['referee']}</div>
           <div id="matchScore">
-            <div>{match['fullTimeHomeTeamGoals']}</div>
-            <div>FT</div>
-            <div>{match['fullTimeAwayTeamGoals']}</div>
+            <div id='matchScoreCol' className="fs-5">{match['fullTimeHomeTeamGoals']}</div>
+            <div id='matchScoreCol' className="fs-5">FT</div>
+            <div id='matchScoreCol' className="fs-5">{match['fullTimeAwayTeamGoals']}</div>
           </div>
         </div>  
-        <div>
+        <div id='matchLogo'>
           <Image src={`/images/${match['awayTeam']}.png`} fluid />
           <p className='my-auto text-light'>{match['awayTeam']}</p>
         </div>
