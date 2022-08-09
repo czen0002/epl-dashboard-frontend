027e83bac6940cfc328ec10665eeb0692fd99c80
czen0002 <zc114789@gmail.com>
2022-08-09 23:33:04 +1000

    [style][CHENG] Extract MatchOnDate style to MatchOnDate.scss

--- src/components/MatchOnDate.js
+++ src/components/MatchOnDate.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import MatchTile from './MatchTile';
+import './MatchOnDate.scss';
 
 export default function MatchOnDate({ listIndex, matchesObject, team, month, season }) {
   const matchesDate = matchesObject['date'];
   const matchesOnDate = matchesObject['matchArray'];
   return (
     <div>
-      <p className='display-4' style={{color: 'white'}}>{matchesDate}</p>
-      <ul key={listIndex} style={{listStyleType: 'none'}}>
+      <p className='display-4'>{matchesDate}</p>
+      <ul key={listIndex}>
         {matchesOnDate.map((match, index) => <MatchTile key={index} match={match} team={team} month={month} season={season}/>)} 
       </ul>
     </div>
